c70a1c42855c5de7bca809eb60e90e2c43da0d06
ademike <adeniyi.michael119@gmail.com>
2022-03-14 18:45:32 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main -  print quote using printf
  * Description: very simple snippet
  * Return: 0
  */
- int main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
